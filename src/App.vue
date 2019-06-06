8b7594d8a72da19582ed92e9ddd4fb8d191aaecf
wangml <2965816256@qq.com>
2019-06-06 15:50:28 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    index 578989
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+//   mounted(){
+//     this.$axios({
+//       url:'https://api.github.com/users',
+//       method:'get',
+//     }).then({
+//       console.log(res);
+//     })
+//   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+// 1.
+import Element from 'element-ui'
+// 2.
+Vue.use(Element);
+
+// 2.引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
